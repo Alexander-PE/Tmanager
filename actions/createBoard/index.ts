8bd548f3a88c37ddf114ac58d585942f499f4878
Alexander-PE <alexander.pereze02@gmail.com>
2024-01-23 13:58:56 -0500

    chore: use action hook

--- actions/createBoard/index.ts
+++ actions/createBoard/index.ts
@@ -1,7 +1,11 @@
 'use server'
 
 import { auth } from "@clerk/nextjs"
-import { InputType } from "./types"
+import { InputType, ReturnType } from "./types"
+import { revalidatePath } from "next/cache"
+import { db } from "@/lib/db"
+import { CreateSafeAction } from "@/lib/create-safe-action"
+import { CreateBoard } from "./schema"
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId } = auth()
@@ -11,5 +15,26 @@ const handler = async (data: InputType): Promise<ReturnType> => {
       error: "You must be logged in to create a board"
     }
   }
-  
+
+  const { title } = data
+  let board
+
+  try {
+    board = await db.board.create({
+      data: {
+        title,
+
+      }
+    })
+  } catch (error) {
+    return {
+      error: "Failed to create board"
+    }
+  }
+
+  revalidatePath(`/board/${board.id}`)
+  return {data: board}
 }
+
+export const createBoard = CreateSafeAction(CreateBoard, handler)
+
--- app/(platform)/(dashboard)/organization/[id]/form.tsx
+++ app/(platform)/(dashboard)/organization/[id]/form.tsx
@@ -1,18 +1,30 @@
 "use client"
 
-import { create } from "@/actions/createBoard"
+import { createBoard } from "@/actions/createBoard"
 import { FormInput } from "./input"
 import { useFormState } from "react-dom"
 import { FormButton } from "./form-button"
+import { useAction } from "@/hooks/use-action"
 
 export const Form = () => {
-  const initialState = { message: null, errors: {} }
-  const [state, dispatch] = useFormState(create, initialState)
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log(data, "success")
+    },
+    onError: (error) => {
+      console.log(error, "error")
+    }
+  })
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string
+
+    execute({title})
+  }
 
   return (
-    <form action={dispatch}>
+    <form action={onSubmit}>
       <div className="flex flex-col space-y-2">
-        <FormInput errors={state?.errors} />
+        <FormInput errors={fieldErrors} />
       </div>
       <FormButton />
     </form>
