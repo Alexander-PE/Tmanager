3cd66b63a3d1762309769ae0fda2bd8e52f0a338
Alexander-PE <alexander.pereze02@gmail.com>
2024-01-24 16:25:12 -0500

    refactor: form components

--- app/(platform)/(dashboard)/organization/[id]/form.tsx
+++ app/(platform)/(dashboard)/organization/[id]/form.tsx
@@ -1,10 +1,10 @@
 "use client"
 
 import { createBoard } from "@/actions/createBoard"
-import { FormInput } from "./input"
 import { useFormState } from "react-dom"
-import { FormButton } from "./form-button"
 import { useAction } from "@/hooks/use-action"
+import { FormInput } from "@/components/form/form-input"
+import { FormSubmit } from "@/components/form/form-submit"
 
 export const Form = () => {
   const { execute, fieldErrors } = useAction(createBoard, {
@@ -18,15 +18,17 @@ export const Form = () => {
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string
 
-    execute({title})
+    execute({ title })
   }
 
   return (
     <form action={onSubmit}>
       <div className="flex flex-col space-y-2">
-        <FormInput errors={fieldErrors} />
+        <FormInput label="Board title" id="title" errors={fieldErrors} />
       </div>
-      <FormButton />
+      <FormSubmit>
+        Save
+      </FormSubmit>
     </form>
   )
 }
