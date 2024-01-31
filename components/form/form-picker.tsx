1275ff7d4fe44510c5651de4c6d50063d5d7278d
Alexander-PE <alexander.pereze02@gmail.com>
2024-01-31 17:59:50 -0500

    chore: update form files

--- components/form/form-picker.tsx
+++ components/form/form-picker.tsx
@@ -2,11 +2,14 @@
 
 import { useEffect, useState } from "react"
 import { unsplash } from "@/lib/unsplash"
-import { Loader2 } from "lucide-react"
+import { Check, Loader2 } from "lucide-react"
 import { useFormStatus } from "react-dom"
 import { cn } from "@/lib/utils"
 import Image from "next/image"
 import { set } from "zod"
+import { defaultImages } from "@/constants/images"
+import Link from "next/link"
+import { FormErrors } from "./form-errors"
 
 interface FormPickerProps {
   id: string
@@ -16,7 +19,7 @@ interface FormPickerProps {
 export const FormPicker = ({ id, errors }: FormPickerProps) => {
   const { pending } = useFormStatus()
 
-  const [images, setImages] = useState<Array<Record<string, any>>>([])
+  const [images, setImages] = useState<Array<Record<string, any>>>(defaultImages)
   const [loading, setLoading] = useState(true)
   const [selectedImageId, setSelectedImageId] = useState(null)
 
@@ -33,7 +36,7 @@ export const FormPicker = ({ id, errors }: FormPickerProps) => {
         }
       } catch (error) {
         console.error(error)
-        setImages([])
+        setImages(defaultImages)
       } finally {
         setLoading(false)
       }
@@ -64,10 +67,32 @@ export const FormPicker = ({ id, errors }: FormPickerProps) => {
               setSelectedImageId(image.id)
             }}
           >
+            <input 
+              type="radio" 
+              id={id} 
+              name={id} 
+              className="hidden" 
+              checked={selectedImageId === image.id} 
+              disabled={pending} 
+              value={`${image.id}|${image.urls.thumb}|${image.urls.full}|${image.links.html}|${image.user.name}`} 
+            />
             <Image src={image.urls.thumb} alt="unsplash image" className="object-cover rounded-sm" fill />
+            {selectedImageId === image.id && (
+              <div className="absolute inset-y-0 h-full w-full bg-black/30 flex items-center justify-center">
+                <Check className="h-4 w-4 text-white" />
+              </div>
+            )}
+            <Link
+              href={image.links.html}
+              target="_blank"
+              className="opacity-0 group-hover:opacity-100 absolute w-full bottom-0 text-[10px] truncate text-white hover:underline p-1 bg-black/50"
+            >
+              {image.user.name}
+            </Link>
           </div>
         ))}
       </div>
+      <FormErrors errors={errors} id="image" />
     </div>
   )
 }
