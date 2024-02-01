3a11e3131eca4150546621e9ab4fc943347bbb2e
Alexander-PE <alexander.pereze02@gmail.com>
2024-02-01 01:33:40 -0500

    chore: update board layout and components

--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db"
 import { auth } from "@clerk/nextjs"
 import { notFound, redirect } from "next/navigation"
+import { BoardNavbar } from "./_components/board-navbar"
 
 export async function generateMetadata({ params }: { params: { boardId: string } }) {
   const { orgId } = auth()
@@ -39,6 +40,8 @@ const BoardIdLayout = async ({ children, params }: { children: React.ReactNode, 
 
   return (
     <div style={{ backgroundImage: `url(${board.imageFullUrl})` }} className="relative h-full bg-no-repeat bg-cover bg-center">
+      <div className="absolute inset-0 bg-black/15" />
+      <BoardNavbar data={board} />
       <main className="relative pt-28 h-full">
         {children}
       </main>
