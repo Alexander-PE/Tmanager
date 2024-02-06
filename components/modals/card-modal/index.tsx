8fb1773d9b0dd81bc52e71b4fe8de5a2d9aaa930
Alexander-PE <alexander.pereze02@gmail.com>
2024-02-05 22:35:38 -0500

    chore: add api routes and card modal

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -2,16 +2,33 @@
 
 import { Dialog, DialogContent } from "@/components/ui/dialog"
 import { useCardModal } from "@/hooks/use-card-modal"
+import { fetching } from "@/lib/fetching"
+import { CardWithList } from "@/types"
+import { useQuery } from "@tanstack/react-query"
+import { Header } from "./header"
+import { Description } from "./description"
 
 export const CardModal = () => {
   const id = useCardModal(state => state.id)
   const isOpen = useCardModal(state => state.isOpen)
   const onClose = useCardModal(state => state.onClose)
 
+  const { data: cardData } = useQuery<CardWithList>({
+    queryKey: ["card", id],
+    queryFn: () => fetching(`/api/cards/${id}`),
+  })
+
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent>
-        I am a modal
+        {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+        <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+          <div className="col-span-3">
+            <div className="w-full space-y-6">
+              {!cardData ? <Description.Skeleton /> : <Description data={cardData} />}
+            </div>
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   )
